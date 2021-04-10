e599668a949dda1e7f85352752cc92f9fd58fcf7
Gerald <gerald_barnert@hotmail.com>
2021-04-10 11:55:57 -0400

    pk class

--- test2.py
+++ test2.py
@@ -5,8 +5,9 @@ from camb import model
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
 pars = camb.CAMBparams()
-pars.set_cosmology(H0=67.5, ombh2=0.022, omch2=0.122)
+pars.set_cosmology(H0=70, ombh2=0.022, omch2=0.122)
 #pars.InitPower.set_params(ns=0.965)
 pars.set_matter_power(redshifts=[1,2,3], kmax=1.0)
 results = camb.get_results(pars)
@@ -16,4 +17,28 @@ plt.loglog(kh, pk[0,:])
 plt.loglog(kh, pk[1,:])
 plt.loglog(kh, pk[2,:])
 plt.show()
+'''
 
+LambdaCDM = Class()
+LambdaCDM.set({'omega_b': 0.022, 'omega_cdm': 0.12, 'h': 0.73, 'z_max_pk':4})
+LambdaCDM.set({'output': 'tCl, lCl, pCl, mPk' , 'lensing':'yes', 'P_k_max_1/Mpc': 3.0 })
+LambdaCDM.compute()
+
+kk = np.linspace(1e-4, 2, 2000)
+Pk = []
+Pk_z = []
+h = LambdaCDM.h()
+zz = np.arange(0,3,1)
+print(zz)
+
+for z in zz:
+    Pk = []
+    for k in kk:
+        Pk.append(LambdaCDM.pk(k*h,z) * h**3)
+    Pk_z.append(Pk)
+
+plt.loglog(kk, Pk_z[0])
+plt.loglog(kk, Pk_z[1])
+plt.loglog(kk, Pk_z[2])
+
+plt.show()
